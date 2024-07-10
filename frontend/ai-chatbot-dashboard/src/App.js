411794a6f8851b281a5c1e39fe07fe4e999c4e81
Nauman Sabir <officialnaumansabir@gmail.com>
2024-07-11 00:34:58 +0400

    Initial setup with Docker, Django, React, and MongoDB

---
+++ frontend/ai-chatbot-dashboard/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          
+        </p>
+        <a
+         
+        >
+          HI
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
